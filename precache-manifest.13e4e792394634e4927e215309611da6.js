d13f798511b004963fdfc0801cc5925a235d8c4b
Dennis Kim <dekim2324@gmail.com>
2020-04-13 18:24:38 -0400

    Updates

---
+++ precache-manifest.13e4e792394634e4927e215309611da6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90844a1b869befe8e4bd570fc6db4522",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "6306afda42a4fb41e187",
+    "url": "/covid-19-tracker/static/css/main.e9541f98.chunk.css"
+  },
+  {
+    "revision": "d244851a9bf63a366e10",
+    "url": "/covid-19-tracker/static/js/2.2b82164c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.2b82164c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6306afda42a4fb41e187",
+    "url": "/covid-19-tracker/static/js/main.ed8dbee0.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/covid-pic.d7265326.png"
+  }
+]);
